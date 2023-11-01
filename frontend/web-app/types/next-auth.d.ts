63be9328b74e17bd37661de314b66ddcc089d9b0
vincenzocaruso <enzo@carusovincenzo.com>
2023-11-01 19:08:45 +0100

    Partial Section 10

--- frontend/web-app/types/next-auth.d.ts
+++ frontend/web-app/types/next-auth.d.ts
@@ -3,12 +3,17 @@ import { DefaultSession } from 'next-auth';
 declare module 'next-auth' {
     interface Session {
         user: {
+            id: string
             username: string
         } & DefaultSession['user']
     }
 
     interface Profile {
         username: stirng
+    }
+
+    interface User {
+        username: string
     }
 }
 
