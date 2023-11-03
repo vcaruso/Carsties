4c19a7a862f99ab637383fb8b0e873af4dcd4554
vincenzocaruso <enzo@carusovincenzo.com>
2023-11-03 17:11:24 +0100

    Add assets

---
+++ src/BiddingService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace BiddingService;
+
+public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
+{
+    public async Task Consume(ConsumeContext<AuctionCreated> context)
+    {
+        var auction = new Auction
+        {
+            ID = context.Message.Id.ToString(),
+            Seller = context.Message.Seller,
+            ReservePrice = context.Message.ReservePrice
+        };
+
+        await auction.SaveAsync();
+    }
+}
---
+++ src/BiddingService/Models/BidStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace BiddingService;
+
+public enum BidStatus
+{
+    Accepted,
+    AcceptedBelowReseve,
+    TooLow,
+    Finished
+
+}
