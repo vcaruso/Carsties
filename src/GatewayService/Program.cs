99a61279dcff48b3548814bece2d08707acf2d9f
vcaruso <vcaruso@xeniaprogetti.it>
2023-11-05 12:46:08 +0100

    End of Section 12

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -16,8 +16,20 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
 
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
